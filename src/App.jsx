3f7eda4b65021d2cce1da966fe568e6cbde7100e
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-04-30 22:15:27 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,47 @@
-import { Suspense, lazy, useEffect } from "react";
+import { lazy, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-
-import Loader from "./components/Loader/Loader";
-
-// const MailboxPage = lazy(() => import("./pages/MailboxPage"));
-// const ProductsPage = lazy(() => import("./pages/ProductsPage"));
-// const SearchPage = lazy(() => import("./pages/SearchPage"));
-const HomePage = lazy(() => import("./pages/HomePage"));
-const NotFound = lazy(() => import("./pages/NotFound"));
-// const ProductDetailsPage = lazy(() => import("./pages/ProductDetailsPage"));
-// TODO: add lazy loading
-import RegistrationPage from "./pages/RegistrationPage";
-import LoginPage from "./pages/LoginPage";
-import ContactsPage from "./pages/ContactsPage";
-import Layout from "./components/Layout/Layout";
 import { useDispatch } from "react-redux";
+import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Layout from "./components/Layout/Layout";
 import { refreshUser } from "./redux/auth/operations";
 
-/*
- Робота з маршрутизацією:
-  1. Навчитися змінювати URL-адресу браузера за допогобо 
-    компонента Link | NavLink.
-  2. Підготувати шаблони компонентів(сторінок) та рендерити
-    їх в залежності від шаблону адреси(pathname) в браузері (Route).
+// import Loader from "./components/Loader/Loader";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const RegistrationPage = lazy(() => import('./pages/RegistrationPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage'));
+const ContactsPage = lazy(() => import('./pages/ContactsPage'));
 
 
-  Компонети Link | NavLink - ми використовуємо для 
-    внутрішньої навігації всередині веб-сторінки.
-  Тег <a> - ми використовуємо для посиланнь на зовніші 
-    ресурси(ютубе, інста, тг, гугл посилання і т.п.).
-    -- target="_blank" rel="noopener noreferrer" --
-*/
 
-function App() {
+function App() { 
   const dispatch = useDispatch();
 
+
   useEffect(() => {
-    dispatch(refreshUser());
+    dispatch(refreshUser());    
   }, [dispatch]);
 
-  return (
-    <Layout>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegistrationPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/contacts" element={<ContactsPage />} />
-
-          
-         
-          
-          
-
-          <Route path="*" element={<NotFound />} />
+  return  (<>
+      <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={
+            <RestrictedRoute>
+              <RegistrationPage />
+            </RestrictedRoute>} />
+        <Route path="/login" element={<RestrictedRoute>
+              <LoginPage />
+            </RestrictedRoute>} />
+          <Route path="/contacts" element={<PrivateRoute><ContactsPage /></PrivateRoute>} />
+          <Route path="*" element={<NotFound/>}/>
         </Routes>
-      </Suspense>
     </Layout>
-  );
+      
+  </>
+  )
 }
 
 export default App;
