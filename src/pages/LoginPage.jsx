7ffe9127fe42ab1b3ac1dd0a62cc9da85bbe9dc5
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-04-30 17:21:02 +0300

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import Loader from '../components/Loader/Loader';
 import LoginForm from '../components/LoginForm/LoginForm'
-import { selectIsError, selectIsLoading } from '../../redux/auth/selectors';
+import { selectIsError, selectIsLoading } from '../redux/auth/selectors';
  
 
 const LoginPage = () => {
