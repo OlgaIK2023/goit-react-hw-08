2fb0ef25cb70be1909ec197305e97d365f088c30
OlgaIK2023 <144039220+OlgaIK2023@users.noreply.github.com>
2024-04-30 15:46:20 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,55 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { contactsReducer } from './contactsSlice';
-import { filtersReducer } from './filtersSlice';
+// import { contactsReducer } from './contactsSlice';
+// import { filtersReducer } from './filtersSlice';
 // import { contactsReducerNew } from "./contactsSliceNew";
+
+
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+
+import { timerReducer } from "./timer/timerSlice";
+
+import { authReducer } from "./auth/slice";
+
+const authPeristConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
+
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts:contactsReducer,
+//     filters: filtersReducer,
+//   },
+// });
 
 export const store = configureStore({
   reducer: {
-    contacts:contactsReducer,
-    filters: filtersReducer,
+    // mailbox: persistReducer(mailboxPeristConfig, mailboxReducer),
+   
+    countDownTimer: timerReducer,
+    
+    auth: persistReducer(authPeristConfig, authReducer),
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
